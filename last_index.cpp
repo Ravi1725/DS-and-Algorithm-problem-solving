0b52e3355d838fee350e564f3959e5458973039b
root <ankitraj1725@gmail.com>
2020-10-10 23:22:13 +0530

    last index by recursion

---
+++ last_index.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int last_index(int arr[],int n,int m,int i,int k)
+{
+	//base case
+	if(n==0)
+	{
+		cout<<k<<endl;
+		return k;
+	}
+
+	//recursive case
+	if(arr[i]==m)
+	{
+		k=i;
+		last_index(arr,n-1,m,i+1,k);
+		cout<<k<<endl;
+	}
+
+	last_index(arr,n-1,m,i+1,k);
+	
+}
+int main() {
+	int n,*arr;
+	int m;
+	cin>>n;
+	arr=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cin>>m;
+	cout<<last_index(arr,n,m,0,-1);
+	return 0;
+}
